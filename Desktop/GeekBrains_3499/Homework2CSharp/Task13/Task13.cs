ad9d27b91d6f949a4333c15624dbed2267aaa56e
Alexander <aromanov999@list.ru>
2022-11-04 20:19:54 +0400

    second commit

---
+++ Desktop/GeekBrains_3499/Homework2CSharp/Task13/Task13.cs
@@ -0,0 +1,39 @@
+﻿int num = 0;
+int number = GetNumber(num);
+int ThirdDigit = GetThirdDigit(number);
+PrintThirdDigit(ThirdDigit);
+
+int GetNumber(int number)
+{
+    Console.WriteLine("Введите число");
+    number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int GetThirdDigit(int num)
+{
+    int ThirdDigit = 0;
+    if(num>-100 && num<100) {
+        ThirdDigit = -1;
+    }
+    else {
+        if (num >= 100) {
+            while (num >= 1000)
+            {
+                num /= 10;
+            }
+            ThirdDigit = num%10;
+        }
+    }
+    return ThirdDigit;
+}
+
+void PrintThirdDigit (int ThirdDigit)
+{
+    if (ThirdDigit>=0 && ThirdDigit<=9) {
+        Console.WriteLine(ThirdDigit);    
+    }
+    else {
+        Console.WriteLine("Третьей цифры нет");
+    } 
+}
