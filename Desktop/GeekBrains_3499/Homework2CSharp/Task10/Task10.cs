53105910764d77779018293b64f9ed1d4cf097e9
Alexander <aromanov999@list.ru>
2022-11-04 19:05:24 +0400

    first commit

---
+++ Desktop/GeekBrains_3499/Homework2CSharp/Task10/Task10.cs
@@ -0,0 +1,29 @@
+﻿int num = 0;
+int number = GetNumber(num);
+int SecondDigit = GetSecondDigit(number);
+PrintSecondDigit(SecondDigit);
+
+int GetNumber(int number)
+{
+    Console.WriteLine("Введите трехзначное число");
+    while (true) {
+        number = int.Parse(Console.ReadLine());
+        if ((number>-100 && number<100) || number>999 || number<-999) {
+            Console.WriteLine("Нужно ввести трехзначное число!");
+        }
+        else {
+            return number;
+        }
+    }
+}
+
+int GetSecondDigit(int num)
+{
+    int SecondDigit = (num/10)%10;
+    return SecondDigit;
+}
+
+void PrintSecondDigit (int SecondDigit)
+{
+    Console.WriteLine(SecondDigit);
+}
