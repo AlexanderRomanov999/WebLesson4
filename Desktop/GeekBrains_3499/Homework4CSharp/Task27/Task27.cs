f37b160d7b412c0b1f1ba2c5c005083e65198566
Alexander <aromanov999@list.ru>
2022-11-06 15:23:26 +0400

    second commit

---
+++ Desktop/GeekBrains_3499/Homework4CSharp/Task27/Task27.cs
@@ -0,0 +1,31 @@
+﻿int num = 0;
+int number = GetNumber(num);
+int Sum = GetSumOfDigits(number);
+PrintSumOfDigits(Sum);
+
+int GetNumber(int number)
+{
+    Console.WriteLine("Введите число");
+    number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int GetSumOfDigits(int num)
+{
+    int Sum = 0;
+    int temp = num;
+    while (num > 0)
+    {
+        temp = num;
+        num %= 10;
+        Sum += num;
+        num = temp;
+        num /= 10;
+    }
+    return Sum;
+}
+
+void PrintSumOfDigits (int Sum)
+{
+    Console.WriteLine(Sum);
+}
