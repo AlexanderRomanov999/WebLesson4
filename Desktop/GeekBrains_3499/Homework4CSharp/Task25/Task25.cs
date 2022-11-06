21fd987d167226fb9c191c6eb950b7719051ee01
Alexander <aromanov999@list.ru>
2022-11-06 14:59:49 +0400

    first commit

---
+++ Desktop/GeekBrains_3499/Homework4CSharp/Task25/Task25.cs
@@ -0,0 +1,23 @@
+﻿int A = 0;
+int B = 0;
+int Power = Exponentiation(A, B);
+PrintResult(Power);
+
+int Exponentiation(int num, int num2)
+{
+    Console.WriteLine("Введите число A");
+    num = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число B");
+    num2 = int.Parse(Console.ReadLine());
+    int Pow = 1;
+    for (int i = 0; i < num2; i++)
+    {
+        Pow *= num;      
+    }
+    return Pow;
+}
+
+void PrintResult(int Pow)
+{
+    Console.WriteLine(Pow);
+}
