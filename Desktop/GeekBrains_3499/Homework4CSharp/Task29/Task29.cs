91ba3e8999ef768c949b19fcd04076385c86724a
Alexander <aromanov999@list.ru>
2022-11-06 15:33:28 +0400

    third commit

---
+++ Desktop/GeekBrains_3499/Homework4CSharp/Task29/Task29.cs
@@ -0,0 +1,31 @@
+﻿int size = 8;
+
+int[] arr = GetRandomArray(size, 1, 101);
+
+PrintArray(arr);
+
+int[] GetRandomArray(int size, int begin, int last) 
+{
+    int[] arr = new int[size];
+    Random random = new Random();
+
+    for (int i=0; i<size; i++)
+    {
+        arr[i]= random.Next(begin, last);
+    } 
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i=0; i<size; i++)
+    {
+        if (i==size-1) {
+             System.Console.Write(arr[i]);   
+        }
+        else {
+            System.Console.Write(arr[i]+", ");
+        }
+    }
+}
