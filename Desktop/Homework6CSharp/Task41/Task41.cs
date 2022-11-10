5704dea5d309e8e5783e928c019a0b5e12aea3fe
Alexander <aromanov999@list.ru>
2022-11-10 19:59:28 +0400

    first commit

---
+++ Desktop/Homework6CSharp/Task41/Task41.cs
@@ -0,0 +1,37 @@
+﻿System.Console.WriteLine("Введите размерность массива");
+int num = Convert.ToInt32(Console.ReadLine());
+double []arr = InitialArr(num);
+int sum = CountPositive(arr);
+PrintArray(arr);
+System.Console.WriteLine("-> " + sum);
+
+double[] InitialArr(int count)
+{
+    double[] arr = new double[count];
+    for (int i = 0; i < count; i++)
+    {
+        arr[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i] + " ");
+    }
+}
+
+int CountPositive(double[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            sum ++;
+        }
+    }
+    return sum;
+}
